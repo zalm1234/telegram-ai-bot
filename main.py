9484a88c01154fef7de27ae3b1f2501c63075256
QuTa <109991678+zalm1234@users.noreply.github.com>
2026-02-12 11:10:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("8289517227:AAGpASlCO7khYMySSZ1vl3CmN4MqQEiMRX8")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is running 🚀")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+app.run_polling()
